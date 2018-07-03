00e38607e8b23fcde3161003e11518545b04cb6e
jianglin <jianglin@192.168.0.114>
2018-07-03 21:55:17 +0800

    测试

--- test/main.js
+++ test/main.js
@@ -4,10 +4,10 @@ import Vue from 'vue';
 import Element from 'element-ui';
 import FHUi from 'fh-ui'; // eslint-disable-line
 import 'element-ui/lib/theme-chalk/index.css';
+import '../lib/static/style.min.css'; // eslint-disable-line
 import App from './App';
 import router from './router';
 import './components';
-import '../lib/static/style.min.css';
 
 Vue.use(Element);
 Vue.use(FHUi);
