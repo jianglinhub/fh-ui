1ab17bc65aa0983771ffe1529994aefc35ab7aa7
Jianglin <jianglin.ren@56qq.com>
2018-07-20 15:58:55 +0800

    test: test eslint

--- test/router/index.js
+++ test/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '../components/HelloWorld';
-import TestAdd from '../components/testadd';
-import TestUpdate from '../components/testupdate';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+import TestAdd from '../components/testadd'
+import TestUpdate from '../components/testupdate'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
@@ -24,4 +24,4 @@ export default new Router({
       component: HelloWorld
     }
   ]
-});
+})
