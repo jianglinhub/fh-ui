af36c4d41f2804f33d6d0e052c11e3f363a83bb6
jianglin <jianglin@192.168.0.109>
2018-09-04 00:05:46 +0800

    feat: 通用查询添加字段多类型查询

--- src/components/base-search/search-sub.vue
+++ src/components/base-search/search-sub.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-      <el-select v-model="options1.fieldCode" placeholder="列表字段" size="small" style="width:160px">
+      <el-select
+        v-model="options1.fieldCode"
+        placeholder="列表字段"
+        size="small"
+        style="width:160px"
+        @change="changeField">
         <el-option
           v-for="item in fields"
           :key="item.value"
@@ -16,8 +21,43 @@
           :value="item.value">
         </el-option>
       </el-select>
-      <el-input placeholder="请输入内容" v-model="options1.defValue" size="small" style="width:160px">
+      <el-input
+        placeholder="请输入内容"
+        v-model="options1.defValue"
+        size="small"
+        style="width:200px"
+        v-if="fieldType === 'string'">
       </el-input>
+      <el-input
+        placeholder="请输入内容"
+        v-model="options1.defValue"
+        size="small"
+        type="number"
+        style="width:200px"
+        v-if="fieldType === 'number'">
+      </el-input>
+      <el-date-picker
+        v-model="options1.defValue"
+        size="small"
+        type="datetime"
+        style="width:200px"
+        placeholder="请选择日期时间"
+        value-format="yyyy-MM-dd hh:mm:ss"
+        v-if="fieldType === 'date'">
+      </el-date-picker>
+      <el-select
+        v-model="options1.defValue"
+        placeholder="请选择"
+        size="small"
+        style="width:200px"
+        v-if="fieldType === 'options'">
+        <el-option
+          v-for="item in fieldOptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
       <el-select v-model="options1.logic" placeholder="并且" size="small" style="width:80px">
         <el-option
           v-for="item in ands"
@@ -33,6 +73,8 @@ export default {
   name: 'searchgSub',
   data() {
     return {
+      fieldType: 'string',
+      fieldOptions: [],
       ands: [{
         value: 'AND',
         label: '并且',
@@ -97,6 +139,13 @@ export default {
           logic: '',
         };
       },
+    },
+  },
+  methods: {
+    changeField(key) {
+      const item = this.fields.filter(x => x.value === key); // 查询找当前key的type
+      this.fieldType = item[0].type ? item[0].type : 'string';
+      this.fieldOptions = item[0].options ? item[0].options : [];
     },
   },
 };
